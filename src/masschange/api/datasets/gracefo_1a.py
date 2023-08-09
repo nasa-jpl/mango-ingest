37319b7482cc21b490439bb349028f2531763bbe
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-08-08 22:17:36 -0700

    minor cleanup

--- src/masschange/api/datasets/gracefo_1a.py
+++ src/masschange/api/datasets/gracefo_1a.py
@@ -1,13 +1,9 @@
-from datetime import date, datetime
-from typing import Dict, Iterable, List, Tuple
+from datetime import datetime
 
 from fastapi import APIRouter, HTTPException
 
 from masschange.datasets.gracefo_1a import GraceFO1AFullResolutionDataset
 from masschange.datasets.timeseriesdataset import TooMuchDataRequestedError
-
-# from masschange.datasets import mappers
-
 
 router = APIRouter(prefix='/GRACEFO-1A')
 
@@ -16,7 +12,6 @@ router = APIRouter(prefix='/GRACEFO-1A')
 async def get_full_resolution_data(
         from_isotimestamp: datetime = datetime.min,
         to_isotimestamp: datetime = datetime.max):
-    exclude_fields = {'rcvtime_frac', 'rcvtime_intg', 'temporal_partition_key'}
     try:
         results = GraceFO1AFullResolutionDataset.select(from_isotimestamp, to_isotimestamp)
     except TooMuchDataRequestedError as err:
@@ -27,9 +22,10 @@ async def get_full_resolution_data(
         'to_dt': to_isotimestamp.isoformat(),
         'data': results
     }
+
+
 @router.get('/fields', tags=['GRACEFO-1A', 'metadata'])
 async def get_available_fields():
-
     return {
         'data': sorted(list(GraceFO1AFullResolutionDataset.get_available_fields()))
     }
