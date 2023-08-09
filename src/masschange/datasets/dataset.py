4bf4ff1270fdf6dfe914fe672ff67347f5b2e241
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-08-08 21:13:23 -0700

    implement pre-pruning pseudoindex optimisation

--- src/masschange/datasets/dataset.py
+++ src/masschange/datasets/dataset.py
@@ -1,9 +1,10 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from datetime import datetime
 
 
 class Dataset(ABC):
     """
-    A parquet-backed dataset.
+    A parquet-backed time-series dataset.
     Attributes:
         - dataset_id: a dataset type identifier for this dataset type
         - parquet_path: a local-filesystem parquet src_filepath (or basepath, if partitioned)
@@ -13,3 +14,8 @@ class Dataset(ABC):
 
     def __init__(self, parquet_path: str):
         self.parquet_path = parquet_path
+
+    @classmethod
+    @abstractmethod
+    def enumerate_temporal_partition_values(cls, from_dt: datetime, to_dt: datetime):
+        pass
