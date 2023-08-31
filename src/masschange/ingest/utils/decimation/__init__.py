9e1e0fb04a02a4f81d96bb9616ee05a432481cba
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-08-31 08:50:23 -0700

    implement AggregationRunConfig

--- src/masschange/ingest/utils/decimation/__init__.py
+++ src/masschange/ingest/utils/decimation/__init__.py
@@ -13,18 +13,19 @@ from pyarrow import compute as pc
 
 from masschange.ingest.utils import get_configured_logger
 from masschange.ingest.utils.benchmarking import get_human_readable_elapsed_since
+from masschange.ingest.utils.decimation.aggregationrunconfig import AggregationRunConfig
 
 log = get_configured_logger()
 
 
-def decimate_file_by_constant_factor(factor: int, agg_config: [Dict[str, List[str | Callable]]], input_path: str,
+def decimate_file_by_constant_factor(factor: int, run_config: AggregationRunConfig, input_path: str,
                                      output_path: str):
     log.info(f'loading data for decimation from {input_path}')
     in_ds = (pq.ParquetDataset(input_path))
     in_table = in_ds.read()
 
     df = in_table.to_pandas()
-    decimated_df = decimate_df_by_constant_factor(factor, agg_config, df)
+    decimated_df = decimate_df_by_constant_factor(factor, run_config, df)
 
     log.info(f'writing decimated output to {output_path}')
     out_table = pa.Table.from_pandas(decimated_df, preserve_index=False)
@@ -33,23 +34,24 @@ def decimate_file_by_constant_factor(factor: int, agg_config: [Dict[str, List[st
     log.info('decimation complete!')
 
 
-def decimate_df_by_constant_factor(factor: int, agg_config, df: pd.DataFrame):
+def decimate_df_by_constant_factor(factor: int, run_config: AggregationRunConfig, df: pd.DataFrame):
     # N.B. THIS WILL RESULT IN CORES-1 ADDITIONAL AGGREGATIONS WITH INCONSISTENT INPUT ROW COUNTS.  THIS IS ACCEPTABLE FOR
     # PLOTTING BUT MAYBE NOT FOR EXACT DATA ANALYSIS.  KEEP AN EYE ON THIS.
 
     # TODO: IMPLEMENT SORTING ON TIMESTAMP TO ENSURE FAST TEMPORAL QUERYING - TEST HOW IMPORTANT THAT IS
     cores = 12
     chunks = np.array_split(df, cores)
-    decimation_f = functools.partial(_decimate_df_by_constant_factor, factor, agg_config)
-    with multiprocessing.Pool(cores)as mp_pool:
+    decimation_f = functools.partial(_decimate_df_by_constant_factor, factor, run_config)
+    with multiprocessing.Pool(cores) as mp_pool:
         decimated_chunks = mp_pool.map(decimation_f, chunks)
 
     result = pd.concat(decimated_chunks)
     return result
 
-def _decimate_df_by_constant_factor(factor: int, agg_config, df: pd.DataFrame):
+
+def _decimate_df_by_constant_factor(factor: int, run_config: AggregationRunConfig, df: pd.DataFrame):
     # TODO: CONVERT df TO np.DataFrame, reinstate satellite_id
-    decimate_f = functools.partial(decimate_rowgroup, agg_config)
+    decimate_f = functools.partial(decimate_rowgroup, run_config)
     # create row-groups every {factor} rows, and apply decimation function to each row-group
     decimated_df = df.groupby(df.index // factor).apply(decimate_f)
 
@@ -58,28 +60,33 @@ def _decimate_df_by_constant_factor(factor: int, agg_config, df: pd.DataFrame):
     return decimated_df
 
 
-def decimate_rowgroup(agg_config, rowgroup):
-    df = rowgroup.agg(agg_config)
-    agg_dict = {}
-    for property in df.columns:
-        for agg_name in df[property].index:
+def decimate_rowgroup(run_config: AggregationRunConfig, rowgroup: pd.DataFrame):
+    # Perform aggregations
+    rowgroup = rowgroup.agg(run_config.aggregation_funcs)
+
+    # Extract values into flat dict column mapping
+    aggregate_dict = {}
+    for property in rowgroup.columns:
+        for agg_name in rowgroup[property].index:
             new_property = f'{property}_{agg_name}'
-            value = df[property][agg_name]
+            value = rowgroup[property][agg_name]
             if not np.isnan(value):
-                agg_dict[new_property] = [value]
+                aggregate_dict[new_property] = [value]
 
-    agg_df = pd.DataFrame.from_dict(agg_dict)
+    df = pd.DataFrame.from_dict(aggregate_dict)
 
-    # hack - need to incorporate this into AggregationConfig post_aggregation_column_casts
-    agg_df['rcvtime_mean'] = agg_df['rcvtime_mean'].astype(int)
-    # hack - need to incorporate this into AggregationConfig post_aggregation_column_renames
-    agg_df.rename(columns={'rcvtime_mean': 'rcvtime'})
+    # Apply column type casts
+    for column, type in run_config.column_type_mappings.items():
+        df[column] = df[column].astype(type)
 
-    return agg_df
+    # Apply column name remappings
+    df = df.rename(columns=run_config.column_name_mappings)
+
+    return df
 
 
 if __name__ == '__main__':
-    agg_config = {
+    aggregation_funcs = {
         'lin_accl_x': ['min', 'max'],
         'lin_accl_y': ['min', 'max'],
         'lin_accl_z': ['min', 'max'],
@@ -88,6 +95,16 @@ if __name__ == '__main__':
         'ang_accl_z': ['min', 'max'],
         'rcvtime': np.mean
     }
+
+    type_conversion_mappings = {
+        'rcvtime_mean': 'long'
+    }
+
+    column_rename_mappings = {
+        'rcvtime_mean': 'rcvtime'
+    }
+
+    run_config = AggregationRunConfig(aggregation_funcs, type_conversion_mappings, column_rename_mappings)
 
     decimation_step_factors = [20, 20, 20, 3]  # results in 1:20, 1:400, 1:8000, 1:24000
     current_decimation_factor = 1
@@ -101,5 +118,5 @@ if __name__ == '__main__':
 
         execution_start = datetime.now()
         log.info(f'Decimating from 1:{current_decimation_factor} to 1:{next_decimation_factor}')
-        decimate_file_by_constant_factor(next_decimation_factor, agg_config, input_root_path, output_filepath)
+        decimate_file_by_constant_factor(next_decimation_factor, run_config, input_root_path, output_filepath)
         log.info(f"decimation of one day's data completed in {get_human_readable_elapsed_since(execution_start)}")
