5f72000e11b70377cac0bc7de9f3002d7f0bb824
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-08-30 19:00:44 -0700

    implement prototype core-parallelism for decimation of data

--- src/masschange/ingest/utils/decimation/__init__.py
+++ src/masschange/ingest/utils/decimation/__init__.py
@@ -1,4 +1,5 @@
 import functools
+import multiprocessing
 import os
 import shutil
 from datetime import datetime
@@ -16,26 +17,45 @@ from masschange.ingest.utils.benchmarking import get_human_readable_elapsed_sinc
 log = get_configured_logger()
 
 
-def decimate_by_constant_factor(factor: int, agg_config: [Dict[str, List[str | Callable]]], input_path: str,
-                                output_path: str):
+def decimate_file_by_constant_factor(factor: int, agg_config: [Dict[str, List[str | Callable]]], input_path: str,
+                                     output_path: str):
     log.info(f'loading data for decimation from {input_path}')
     in_ds = (pq.ParquetDataset(input_path))
     in_table = in_ds.read()
 
     df = in_table.to_pandas()
-
-    decimate_f = functools.partial(decimate_rowgroup, agg_config)
-    # create row-groups every {factor} rows, and apply decimation function to each row-group
-    decimated_df = df.groupby(df.index // factor).apply(decimate_f)
-
-    # hack - this needs to be a partition key.  Assumes file is homogeneous wrt satellite-id, which is currently valid
-    decimated_df['satellite_id'] = df['satellite_id'][0]
+    decimated_df = decimate_df_by_constant_factor(factor, agg_config, df)
 
     log.info(f'writing decimated output to {output_path}')
     out_table = pa.Table.from_pandas(decimated_df, preserve_index=False)
     pq.write_table(out_table, output_path)
 
     log.info('decimation complete!')
+
+
+def decimate_df_by_constant_factor(factor: int, agg_config, df: pd.DataFrame):
+    # N.B. THIS WILL RESULT IN CORES-1 ADDITIONAL AGGREGATIONS WITH INCONSISTENT INPUT ROW COUNTS.  THIS IS ACCEPTABLE FOR
+    # PLOTTING BUT MAYBE NOT FOR EXACT DATA ANALYSIS.  KEEP AN EYE ON THIS.
+
+    # TODO: IMPLEMENT SORTING ON TIMESTAMP TO ENSURE FAST TEMPORAL QUERYING - TEST HOW IMPORTANT THAT IS
+    cores = 12
+    chunks = np.array_split(df, cores)
+    decimation_f = functools.partial(_decimate_df_by_constant_factor, factor, agg_config)
+    with multiprocessing.Pool(cores)as mp_pool:
+        decimated_chunks = mp_pool.map(decimation_f, chunks)
+
+    result = pd.concat(decimated_chunks)
+    return result
+
+def _decimate_df_by_constant_factor(factor: int, agg_config, df: pd.DataFrame):
+    # TODO: CONVERT df TO np.DataFrame, reinstate satellite_id
+    decimate_f = functools.partial(decimate_rowgroup, agg_config)
+    # create row-groups every {factor} rows, and apply decimation function to each row-group
+    decimated_df = df.groupby(df.index // factor).apply(decimate_f)
+
+    # hack - this needs to be a partition key.  Assumes file is homogeneous wrt satellite-id, which is currently valid
+    # decimated_df['satellite_id'] = df['satellite_id'][0]
+    return decimated_df
 
 
 def decimate_rowgroup(agg_config, rowgroup):
@@ -50,9 +70,7 @@ def decimate_rowgroup(agg_config, rowgroup):
 
     agg_df = pd.DataFrame.from_dict(agg_dict)
 
-
-
-    #hack - need to incorporate this into AggregationConfig post_aggregation_column_casts
+    # hack - need to incorporate this into AggregationConfig post_aggregation_column_casts
     agg_df['rcvtime_mean'] = agg_df['rcvtime_mean'].astype(int)
     # hack - need to incorporate this into AggregationConfig post_aggregation_column_renames
     agg_df.rename(columns={'rcvtime_mean': 'rcvtime'})
@@ -83,7 +101,5 @@ if __name__ == '__main__':
 
         execution_start = datetime.now()
         log.info(f'Decimating from 1:{current_decimation_factor} to 1:{next_decimation_factor}')
-        decimate_by_constant_factor(next_decimation_factor, agg_config, input_root_path, output_filepath)
+        decimate_file_by_constant_factor(next_decimation_factor, agg_config, input_root_path, output_filepath)
         log.info(f"decimation of one day's data completed in {get_human_readable_elapsed_since(execution_start)}")
-
-
