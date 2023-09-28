c942d4fc2453f7f229ef3ec4067b26a566d289ff
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-09-28 15:21:18 -0700

    implement simple benchmarking decorator

--- src/masschange/ingest/utils/benchmarking.py
+++ src/masschange/ingest/utils/benchmarking.py
@@ -1,4 +1,23 @@
+import functools
+import logging
 from datetime import datetime
+from typing import Callable
+
+
+def log_elapsed_time(label: str = None, log_f: Callable = logging.getLogger().info):
+    def inner(f):
+        task_label = label or f'{f.__name__}()'
+
+        @functools.wraps(f)
+        def wrapper():
+            begin = datetime.now()
+            result = f()
+            log_f(f'{task_label} completed in {get_human_readable_elapsed_since(begin)}')
+            return result
+
+        return wrapper
+
+    return inner
 
 
 def get_human_readable_elapsed_since(begin: datetime) -> str:
