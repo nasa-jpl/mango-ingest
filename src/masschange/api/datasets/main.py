e140a23a6e0ca1d2a2e90d9515a16b9cc4d10b70
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-11-03 12:56:00 -0700

    implement temporary gracefo_acc1a_lttb dataset/api

--- src/masschange/api/datasets/main.py
+++ src/masschange/api/datasets/main.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from masschange.api.datasets import gracefo_1a
+from masschange.api.datasets import gracefo_acc1a_lttb
 
 router = APIRouter(prefix='/datasets')
 
 router.include_router(gracefo_1a.router)
+router.include_router(gracefo_acc1a_lttb.router)
 
