e11f4d3ad41e0e21f1840390d39bb97f3d0f5714
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-12-22 12:02:57 -0800

    add useful defaults to API docs

--- src/masschange/api/timeseriesdatasetrouterconstructor.py
+++ src/masschange/api/timeseriesdatasetrouterconstructor.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from typing import Type
 
 from fastapi import APIRouter, HTTPException
+from strenum import StrEnum  # only supported in stdlib from Python 3.11 onward
 
 from masschange.datasets.timeseriesdataset import TimeSeriesDataset
 
@@ -9,14 +10,18 @@ from masschange.datasets.timeseriesdataset import TimeSeriesDataset
 def construct_router(DatasetCls: Type[TimeSeriesDataset]) -> APIRouter:
     router = APIRouter(prefix=f'/{DatasetCls.id_suffix}')
 
+    StreamEnum = StrEnum('Stream', sorted(DatasetCls.stream_ids))
+
     @router.get('/streams/{stream_id}/data', tags=[DatasetCls.mission.id, DatasetCls.get_full_id(), 'data'])
     async def get_data(
-            stream_id: str,
-            from_isotimestamp: datetime = datetime.min,
-            to_isotimestamp: datetime = datetime.max):
+            stream_id: StreamEnum,
+            # default values are chosen to allow users to immediately run a fast query from docs page
+            # these may be removed later if they are confusing
+            from_isotimestamp: datetime = DatasetCls.get_data_begin(sorted(DatasetCls.stream_ids)[0]),
+            to_isotimestamp: datetime = DatasetCls.get_data_begin(sorted(DatasetCls.stream_ids)[0]) + timedelta(minutes=1)):
 
         try:
-            results = DatasetCls.select(stream_id, from_isotimestamp, to_isotimestamp, )
+            results = DatasetCls.select(stream_id.name, from_isotimestamp, to_isotimestamp, )
         except Exception as err:  # TODO: Make this specific
             raise HTTPException(status_code=400, detail=str(err))
 
