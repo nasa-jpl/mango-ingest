165930b910525d60c5f2743aeb25866c62851342
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-12-22 16:16:00 -0800

    load database connection values from project-level configuration

--- src/masschange/db/__init__.py
+++ src/masschange/db/__init__.py
@@ -1,5 +1,15 @@
 import psycopg2
 
+from masschange.utils.config import get_config
 
 def get_db_connection():
-    return psycopg2.connect(database='masschange', user='postgres', password='password', host='localhost', port=5433)
+    config = get_config()
+
+    db_config = config['database']
+    host = db_config['host']
+    port = int(db_config['port'])
+    user = db_config['user']
+    password = db_config['password']
+    database = db_config['database']
+
+    return psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
