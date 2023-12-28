6f38d17e38551854ef53e272d2f4cf06b3410d67
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2023-12-27 20:39:12 -0800

    switch db config to use environment variables

--- src/masschange/db/__init__.py
+++ src/masschange/db/__init__.py
@@ -1,15 +1,13 @@
+import os
+
 import psycopg2
 
-from masschange.utils.config import get_config
 
-def get_db_connection():
-    config = get_config()
+def get_db_connection(without_db: bool = False):
+    host = os.environ['TSDB_HOST']
+    port = int(os.environ['TSDB_PORT'])
+    user = os.environ['TSDB_USER']
+    password = os.environ['TSDB_PASSWORD']
+    database = os.environ['TSDB_DATABASE']
 
-    db_config = config['database']
-    host = db_config['host']
-    port = int(db_config['port'])
-    user = db_config['user']
-    password = db_config['password']
-    database = db_config['database']
-
-    return psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    return psycopg2.connect(database=None if without_db else database, user=user, password=password, host=host, port=port)
