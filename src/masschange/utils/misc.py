b94a56854a7c693b4aa6d93696b602104a2e9473
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2024-01-23 13:21:01 -0800

    move get_random_hex_id() to utils.misc

--- src/masschange/utils/misc.py
+++ src/masschange/utils/misc.py
@@ -1,3 +1,4 @@
+import random
 from datetime import timedelta
 
 
@@ -7,3 +8,8 @@ def get_human_readable_timedelta(td: timedelta) -> str:
     m = int(total_seconds % 3600 / 60)
     s = int(total_seconds % 60)
     return (f"{h}h" if h else "") + (f"{m}m" if m else "") + f"{s}s"
+
+
+def get_random_hex_id(id_len: int = 6) -> str:
+    val = random.randint(0, 16 ** id_len)
+    return hex(val)[2:]
