cf0bca3e6b8845b5a27d783c973bb716dad5d4a5
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2024-01-23 15:04:42 -0800

    implement base-class implementation of DataFileReader.parse_data_span() works for daily granules with format YYYY-MM-DD

--- src/masschange/ingest/datafilereaders/base.py
+++ src/masschange/ingest/datafilereaders/base.py
@@ -1,5 +1,6 @@
 import os
 import re
+import logging
 from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
 from typing import Sequence, Dict, Any
@@ -46,13 +47,32 @@ class DataFileReader(ABC):
         pass
 
     @classmethod
-    @abstractmethod
     def parse_data_span(cls, filepath: str) -> TimeSpan:
         """
         Given a path to a data file, parse the timespan of the contained data.
         This is used to delete data prior to overwrite, if necessary
+
+        This default implementation is for daily granules labeled with YYYY-MM-DD, and may need to be overridden in some
+        subclasses or generalized further
         """
-        pass
+        match_patterns = [cls.get_input_file_default_regex(), cls.get_zipped_input_file_default_regex()]
+        match_group_name = 'date_str'
+        filename = os.path.split(filepath)[-1]
+
+        for pattern in match_patterns:
+            match = re.match(pattern, filename)
+            if not match:
+                continue
+
+            try:
+                span_start_str = match.group(match_group_name)
+                span_start = datetime.strptime(span_start_str, '%Y-%m-%d')
+                return TimeSpan(begin=span_start, duration=timedelta(days=1))  # end-exclusive
+            except IndexError:
+                raise NotImplementedError(f'regex match group "{match_group_name}" not defined for one or more input file patterns in {cls.__name__}')
+
+        # if no match is found
+        raise ValueError(f'Failed to match filename {filename} using available match patterns in {cls.__name__}: {match_patterns}')
 
 
 class AsciiDataFileReader(DataFileReader):
