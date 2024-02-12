60b38a5d1de62c4d3a766032f3026e273dd984c2
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2024-02-12 11:50:06 -0800

    implement ingest.utils.enumeration.order_filepaths_by_filename()

--- src/masschange/ingest/utils/enumeration.py
+++ src/masschange/ingest/utils/enumeration.py
@@ -3,7 +3,8 @@ import re
 from typing import Iterable
 
 
-def enumerate_files_in_dir_tree(root_dir: str, match_regex: str | None = None, match_filename_only: bool = False, followlinks: bool = True) -> Iterable[str]:
+def enumerate_files_in_dir_tree(root_dir: str, match_regex: str | None = None, match_filename_only: bool = False,
+                                followlinks: bool = True) -> Iterable[str]:
     """
     Given a directory root and a regex match pattern, return all matching filepaths under it.
 
@@ -26,3 +27,11 @@ def enumerate_files_in_dir_tree(root_dir: str, match_regex: str | None = None, m
 
             if match_regex is None or re.match(match_regex, match_target):
                 yield filepath
+
+
+def order_filepaths_by_filename(filepaths: Iterable[str]) -> Iterable[str]:
+    """
+    Provide crude temporal ordering of input files by sorting them on filename. Timescaledb is optimised for insertion
+    of most-recent data to a given table, so it is desirable to sort them during ingestion.
+    """
+    return sorted(filepaths, key=lambda fp: os.path.split(fp)[-1])
