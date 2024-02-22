e1c9de85b8d6d782b51bf0ce7821374986fcd084
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2024-02-22 13:09:52 -0800

    implement aggregations in field definitions

--- src/masschange/datasets/timeseriesdatasetfield.py
+++ src/masschange/datasets/timeseriesdatasetfield.py
@@ -1,4 +1,7 @@
+from collections.abc import Set, Collection
 from typing import Union, Any
+
+from strenum import StrEnum
 
 
 class TimeSeriesDatasetField:
@@ -9,15 +12,24 @@ class TimeSeriesDatasetField:
         name (str): the field's name, as used in the database and presented via the API
         const_value (Any | None): an optional value for the column, which is assumed to be constant across every datum '
         of a given data product, which is validated during ingestion
+        VALID_AGGREGATIONS (StrEnum): a set of enumerated aggregations which may
 
     """
 
     name: str
     const_value: Union[Any, None]
+    aggregations: Set[str] = set()
 
-    def __init__(self, name: str, const_value: Union[Any, None] = None):
+    VALID_AGGREGATIONS: Set[str] = {'MIN', 'MAX', 'AVG'}
+
+    def __init__(self, name: str, aggregations: Collection[str] = None, const_value: Union[Any, None] = None):
         self.name = name
         self.const_value = const_value
+
+        if aggregations is not None:
+            if not all([agg in self.VALID_AGGREGATIONS for agg in aggregations]):
+                raise ValueError(f'aggregations arg must be subset of {self.VALID_AGGREGATIONS} (got {aggregations})')
+            self.aggregations = set(aggregations)
 
     @property
     def is_constant(self):
