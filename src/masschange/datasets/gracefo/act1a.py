5fdee9d45b9602246e3acf6d22aa2a67ba886bb4
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2024-02-22 14:34:04 -0800

    [needs checking] implement TimeSeriesDataset.time_series_interval

--- src/masschange/datasets/gracefo/act1a.py
+++ src/masschange/datasets/gracefo/act1a.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from masschange.ingest.datafilereaders.base import DataFileReader
 from masschange.ingest.datafilereaders.gracefoact1a import GraceFOAct1ADataFileReader
 from masschange.missions import GraceFO
@@ -12,6 +14,7 @@ class GraceFOAct1ADataset(TimeSeriesDataset):
     mission = GraceFO
     id_suffix = 'ACT1A'
     stream_ids = {'C', 'D'}
+    time_series_interval = timedelta(milliseconds=100)  # TODO: CONFIRM CORRECT VALUE
 
     @classmethod
     def _get_sql_table_schema(cls) -> str:
