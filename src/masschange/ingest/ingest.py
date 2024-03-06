bb524ecd4d14bc5fd8091a121e8e8c8ae019eea0
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2024-03-05 17:15:30 -0800

    move ingest.resolve_dataset() to datasets.utils

--- src/masschange/ingest/ingest.py
+++ src/masschange/ingest/ingest.py
@@ -12,18 +12,8 @@ import pandas
 import pandas as pd
 import psycopg2
 
-from masschange.datasets.gracefo.acc1a import GraceFOAcc1ADataset
-from masschange.datasets.gracefo.act1a import GraceFOAct1ADataset
-from masschange.datasets.gracefo.ihk1a import GraceFOIhk1ADataset
-from masschange.datasets.gracefo.imu1a import GraceFOImu1ADataset
-from masschange.datasets.gracefo.mag1a import GraceFOMag1ADataset
-from masschange.datasets.gracefo.pci1a import GraceFOPci1ADataset
-from masschange.datasets.gracefo.sca1a import GraceFOSca1ADataset
-from masschange.datasets.gracefo.thr1a import GraceFOThr1ADataset
-
-from masschange.datasets.gracefo.act1b import GraceFOAct1BDataset
-
 from masschange.datasets.timeseriesdataset import TimeSeriesDataset
+from masschange.datasets.utils import resolve_dataset
 from masschange.db import get_db_connection
 from masschange.ingest.utils.benchmarking import get_human_readable_elapsed_since
 from masschange.ingest.utils.caggs import refresh_continuous_aggregates
@@ -151,31 +141,6 @@ def ingest_file_to_db(dataset: TimeSeriesDataset, src_filepath: str):
         log.debug(f'ingested file: {src_filepath}')
     else:
         log.info(f'ingested file: {os.path.split(src_filepath)[-1]}')
-
-
-def resolve_dataset(dataset_id: str) -> TimeSeriesDataset:
-    #     hardcode these for now, figure out how to generate them later
-    mappings = {
-        'GRACEFO_ACC1A': GraceFOAcc1ADataset,
-        'GRACEFO_ACT1A': GraceFOAct1ADataset,
-        'GRACEFO_IHK1A': GraceFOIhk1ADataset,
-        'GRACEFO_IMU1A': GraceFOImu1ADataset,
-        'GRACEFO_MAG1A': GraceFOMag1ADataset,
-        'GRACEFO_PCI1A': GraceFOPci1ADataset,
-        'GRACEFO_SCA1A': GraceFOSca1ADataset,
-        'GRACEFO_THR1A': GraceFOThr1ADataset,
-
-        'GRACEFO_ACT1B': GraceFOAct1BDataset
-
-    }
-
-    cls = mappings.get(dataset_id)()
-    if cls is not None:
-        return cls
-    else:
-        err_msg = f"Failed to resolve provided dataset_id (got '{dataset_id}', expected one of {sorted(mappings.keys())})"
-        log.error(err_msg)
-        raise ValueError(err_msg)
 
 
 def get_args() -> argparse.Namespace:
