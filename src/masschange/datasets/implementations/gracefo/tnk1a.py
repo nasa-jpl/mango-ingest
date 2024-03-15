31290946b60f5ff4a918471f5aa3548442106130
ira <ira@jpl.nasa.gov>
2024-03-15 16:35:25 -0700

    90-create-tnk1a-reader

---
+++ src/masschange/datasets/implementations/gracefo/tnk1a.py
@@ -0,0 +1,36 @@
+from masschange.ingest.datafilereaders.base import DataFileReader
+from masschange.ingest.datafilereaders.gracefotnk1a import GraceFOTnk1ADataFileReader
+from masschange.missions import GraceFO
+from masschange.datasets.timeseriesdataset import TimeSeriesDataset
+
+
+class GraceFOTnk1ADataset(TimeSeriesDataset):
+    @classmethod
+    def get_reader(cls) -> DataFileReader:
+        return GraceFOTnk1ADataFileReader()
+
+    mission = GraceFO
+    id_suffix = 'TNK1A'
+    stream_ids = {'C', 'D'}
+
+    @classmethod
+    def _get_sql_table_schema(cls) -> str:
+        return f"""
+            time_intg bigint not null,
+            time_frac int not null,
+            
+            GRACEFO_id CHAR not null,
+            tank_id int not null,
+            qualflg VARCHAR(8) not null,
+            prod_flag VARCHAR(32) not null,
+            
+            timestamp timestamptz not null,
+            
+            tank_pres double precision,
+            reg_pres double precision,
+            skin_temp double precision,
+            skin_temp_r double precision,
+            adap_temp double precision,
+            boss_fixed double precision,
+            boss_sliding double precision
+        """
