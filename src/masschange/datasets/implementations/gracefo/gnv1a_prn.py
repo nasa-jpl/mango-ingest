244a1bac04fd15de4768c86c4c90199573ebe968
ira <ira@jpl.nasa.gov>
2024-04-04 13:10:50 -0700

    96-create-gnv1a-reader

---
+++ src/masschange/datasets/implementations/gracefo/gnv1a_prn.py
@@ -0,0 +1,31 @@
+from datetime import timedelta
+
+from masschange.ingest.datafilereaders.base import DataFileReader
+from masschange.ingest.datafilereaders.gracefognv1a_prn import GraceFOGnv1APrnDataFileReader
+from masschange.missions import GraceFO
+from masschange.datasets.timeseriesdataset import TimeSeriesDataset
+
+
+class GraceFOGnv1APrnDataset(TimeSeriesDataset):
+    @classmethod
+    def get_reader(cls) -> DataFileReader:
+        return GraceFOGnv1APrnDataFileReader()
+
+    mission = GraceFO
+    id_suffix = 'GNV1A_PRN'
+    stream_ids = {'C', 'D'}
+    time_series_interval = timedelta(seconds=2)
+
+    @classmethod
+    def _get_sql_table_schema(cls) -> str:
+        return f"""
+            rcv_time bigint not null,
+            n_prns int not null,
+            GRACEFO_id CHAR not null,
+
+            prn_id  double precision not null,
+            el_prn  double precision not null,
+            az_prn  double precision not null,
+
+            timestamp timestamptz not null
+        """
