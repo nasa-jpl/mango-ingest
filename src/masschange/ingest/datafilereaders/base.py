1a3ae1b8f27aa98d8ae56d63383174005de65547
ira <ira@jpl.nasa.gov>
2024-04-29 13:08:14 -0700

    110-add-ascendingdescending-column-to-gnv1a

--- src/masschange/ingest/datafilereaders/base.py
+++ src/masschange/ingest/datafilereaders/base.py
@@ -122,7 +122,7 @@ class AsciiDataFileReader(DataFileReader):
 
         # TODO this is a temporary solution until we figure out a generic way to
         # to add geolocation to a dataset
-        cls.append_location(df)
+        cls.append_location_fields(df)
 
         df['timestamp'] = df.apply(cls.populate_timestamp, axis=1)
 
@@ -137,9 +137,10 @@ class AsciiDataFileReader(DataFileReader):
         pass
 
     @classmethod
-    def append_location(cls, df):
+    def append_location_fields(cls, df):
         """
-        Append a location column to the data frame.
+        Append a location and orbit_direction (ascending or descending flag)
+        columns to the data frame.
 
         Parameters
         ----------
