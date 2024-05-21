2a09f943ad54ff1f0a764b3031852c33eec3eaf4
edunn <alexander.e.dunn@jpl.nasa.gov>
2024-05-21 11:48:33 -0700

    update API tests to use new abstractions from other branch

--- src/masschange/api/tests/test_api_ops.py
+++ src/masschange/api/tests/test_api_ops.py
@@ -3,9 +3,9 @@ from fastapi.testclient import TestClient
 from datetime import datetime, timedelta
 
 from masschange.api.app import app
-from masschange.api.tests.utils import is_nearly_equal, permute_all_dataset_instances
-from masschange.datasets.timeseriesdataset import TimeSeriesDataset
-from masschange.datasets.timeseriesdatasetversion import TimeSeriesDatasetVersion
+from masschange.api.tests.utils import is_nearly_equal, permute_all_datasets
+from masschange.dataproducts.timeseriesdataset import TimeSeriesDataset
+from masschange.dataproducts.timeseriesdatasetversion import TimeSeriesDatasetVersion
 
 client = TestClient(app)
 
@@ -15,14 +15,14 @@ def test_root():
     assert response.status_code == 200
 
 
-@pytest.mark.parametrize("ds, version, stream_id", permute_all_dataset_instances())
-def test_gracefo_data_select(ds: TimeSeriesDataset, version: TimeSeriesDatasetVersion, stream_id: str):
+@pytest.mark.parametrize("ds", permute_all_datasets())
+def test_gracefo_data_select(ds: TimeSeriesDataset):
 
-    data_span = ds.get_data_span(version, stream_id)
+    data_span = ds.get_data_span()
     test_span_begin = data_span.begin if data_span is not None else datetime(2000, 1, 1)
     test_span_end = test_span_begin + timedelta(minutes=1)
-    print(f'test_gracefo_data_select() for {ds.get_full_id()} version {version} stream {stream_id}')
-    path = f'/missions/{ds.mission.id}/datasets/{ds.id_suffix}/versions/{version}/streams/{stream_id}/data?fromisotimestamp={test_span_begin.isoformat()}&toisotimestamp={test_span_end.isoformat()}'
+    print(f'test_gracefo_data_select() for {ds.product.get_full_id()} version {ds.version} stream {ds.stream_id}')
+    path = f'/missions/{ds.product.mission.id}/datasets/{ds.product.id_suffix}/versions/{ds.version}/streams/{ds.stream_id}/data?fromisotimestamp={test_span_begin.isoformat()}&toisotimestamp={test_span_end.isoformat()}'
     response = client.get(path)
     assert response.status_code == 200
     content = response.json()
@@ -30,6 +30,6 @@ def test_gracefo_data_select(ds: TimeSeriesDataset, version: TimeSeriesDatasetVe
     # Omit variable-data-span-datasets from the test as the expected data count is unknown
     # Currently, anything not 1Hz or 10Hz is assumed to be variable, though this is not always true
     # TODO: Update once variable data span is implemented properly in dataset classes
-    if data_span is not None and (ds.time_series_interval == timedelta(
-            milliseconds=100) or ds.time_series_interval == timedelta(seconds=1)):
+    if data_span is not None and (ds.product.time_series_interval == timedelta(
+            milliseconds=100) or ds.product.time_series_interval == timedelta(seconds=1)):
         assert is_nearly_equal(600, content['data_count'])
--- src/masschange/api/tests/utils.py
+++ src/masschange/api/tests/utils.py
@@ -1,15 +1,14 @@
-from typing import Union, Iterable, Tuple
+from typing import Union, Iterable
 
-from masschange.datasets.timeseriesdataset import TimeSeriesDataset
-from masschange.datasets.timeseriesdatasetversion import TimeSeriesDatasetVersion
-from masschange.datasets.utils import get_time_series_dataset_classes
+from masschange.dataproducts.timeseriesdataset import TimeSeriesDataset
+from masschange.dataproducts.utils import get_time_series_dataproduct_classes
 
 
-def permute_all_dataset_instances() -> Iterable[Tuple[TimeSeriesDataset, TimeSeriesDatasetVersion, str]]:
-    for ds in get_time_series_dataset_classes():
-        for version in ds.get_available_versions():
-            for stream_id in ds.stream_ids:
-                yield (ds, version, stream_id)
+def permute_all_datasets() -> Iterable[TimeSeriesDataset]:
+    for product in get_time_series_dataproduct_classes():
+        for version in product.get_available_versions():
+            for stream_id in product.stream_ids:
+                yield TimeSeriesDataset(product(), version, stream_id)
 
 
 def is_nearly_equal(expected: Union[int, float], actual: Union[int, float], allowed_deviation_percent=5) -> bool:
