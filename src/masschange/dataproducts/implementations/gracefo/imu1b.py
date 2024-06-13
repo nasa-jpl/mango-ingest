46d53f702658f136fcbec6cecb25fc107548b675
ira <ira@jpl.nasa.gov>
2024-06-13 15:26:39 -0700

    148-create-imu1b-reader

--- src/masschange/dataproducts/implementations/gracefo/imu1b.py
+++ src/masschange/dataproducts/implementations/gracefo/imu1b.py
@@ -1,21 +1,21 @@
 from datetime import timedelta
 
 from masschange.ingest.datafilereaders.base import DataFileReader
-from masschange.ingest.datafilereaders.gracefoimu1a import GraceFOImu1ADataFileReader
+from masschange.ingest.datafilereaders.gracefoimu1b import GraceFOImu1BDataFileReader
 from masschange.missions import GraceFO
 from masschange.dataproducts.timeseriesdataproduct import TimeSeriesDataProduct
 
 
-class GraceFOImu1ADataProduct(TimeSeriesDataProduct):
+class GraceFOImu1BDataProduct(TimeSeriesDataProduct):
     @classmethod
     def get_reader(cls) -> DataFileReader:
-        return GraceFOImu1ADataFileReader()
+        return GraceFOImu1BDataFileReader()
 
     mission = GraceFO
-    id_suffix = 'IMU1A'
+    id_suffix = 'IMU1B'
     stream_ids = {'C', 'D'}
     time_series_interval = timedelta(seconds=1/8) # 8Hz, three gyros
-    processing_level = '1A'
+    processing_level = '1B'
 
     @classmethod
     def get_sql_table_schema(cls) -> str:
