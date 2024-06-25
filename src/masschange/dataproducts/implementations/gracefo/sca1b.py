e17576d3d5a6b725d21f323a4d540a9644092aa7
edunn <alexander.e.dunn@jpl.nasa.gov>
2024-06-24 18:27:54 -0700

    update new products stream->instrument

--- src/masschange/dataproducts/implementations/gracefo/sca1b.py
+++ src/masschange/dataproducts/implementations/gracefo/sca1b.py
@@ -13,7 +13,7 @@ class GraceFOSca1BDataProduct(TimeSeriesDataProduct):
 
     mission = GraceFO
     id_suffix = 'SCA1B'
-    stream_ids = {'C', 'D'}
+    instrument_ids = {'C', 'D'}
     time_series_interval = timedelta(seconds=1)
     processing_level = '1B'
 
