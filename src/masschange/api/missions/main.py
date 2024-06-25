75f842f04a345bf45f420e3e1a9baf631cda1fcd
edunn <alexander.e.dunn@jpl.nasa.gov>
2024-06-24 19:32:23 -0700

    rename /datasets route to /products

--- src/masschange/api/missions/main.py
+++ src/masschange/api/missions/main.py
@@ -12,7 +12,7 @@ from masschange.missions import Mission
 
 missions: Iterable[Type[Mission]] = {dataset.mission for dataset in get_time_series_dataproduct_classes()}
 
-# Constructs routing for everything in the /missions/{id}/datasets/{id} tree
+# Constructs routing for everything in the /missions/{id}/products/{id} tree
 # This isn't super-clean, but saves having an equivalent "vine" of two-line files to navigate through.
 # Feel free to break it out into an import tree if it gets complicated when non-TimeSeriesDataset stuff is implemented
 missions_router = APIRouter(prefix='/missions')
@@ -20,7 +20,7 @@ missions_router = APIRouter(prefix='/missions')
 for mission in missions:
     mission_router = APIRouter(prefix=f'/{mission.id}')
 
-    mission_data_products_router = APIRouter(prefix='/datasets')
+    mission_data_products_router = APIRouter(prefix='/products')
 
     mission_data_products = [product() for product in get_time_series_dataproduct_classes() if product.mission == mission]
 
