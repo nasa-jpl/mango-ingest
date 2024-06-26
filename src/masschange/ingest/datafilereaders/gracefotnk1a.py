8cb590cb17fc52f1298e500535a1f4036239f6f7
edunn <alexander.e.dunn@jpl.nasa.gov>
2024-06-25 17:47:07 -0700

    update aggs to reflect new lowercase standard

--- src/masschange/ingest/datafilereaders/gracefotnk1a.py
+++ src/masschange/ingest/datafilereaders/gracefotnk1a.py
@@ -34,19 +34,19 @@ class GraceFOTnk1ADataFileReader(DataFileWithProdFlagReader):
             # skip definitions of columns defined by 'prod_flag'
             # add definitions for VariableSchemaAsciiDataFileReaderColumns
             VariableSchemaAsciiDataFileReaderColumn(prod_flag_bit_index=0, name='tank_pres', np_type=np.double,
-                                                    unit='bar', aggregations=['MIN', 'MAX']),
+                                                    unit='bar', aggregations=['min', 'max']),
             VariableSchemaAsciiDataFileReaderColumn(prod_flag_bit_index=1, name='reg_pres', np_type=np.double,
-                                                    unit='bar', aggregations=['MIN', 'MAX']),
+                                                    unit='bar', aggregations=['min', 'max']),
             VariableSchemaAsciiDataFileReaderColumn(prod_flag_bit_index=2, name='skin_temp', np_type=np.double,
-                                                    unit='degrees C', aggregations=['MIN', 'MAX']),
+                                                    unit='degrees C', aggregations=['min', 'max']),
             VariableSchemaAsciiDataFileReaderColumn(prod_flag_bit_index=3, name='skin_temp_r', np_type=np.double,
-                                                    unit='degrees C', aggregations=['MIN', 'MAX']),
+                                                    unit='degrees C', aggregations=['min', 'max']),
             VariableSchemaAsciiDataFileReaderColumn(prod_flag_bit_index=4, name='adap_temp', np_type=np.double,
-                                                    unit='degrees C', aggregations=['MIN', 'MAX']),
+                                                    unit='degrees C', aggregations=['min', 'max']),
             VariableSchemaAsciiDataFileReaderColumn(prod_flag_bit_index=5, name='boss_fixed', np_type=np.double,
-                                                    unit='degrees C', aggregations=['MIN', 'MAX']),
+                                                    unit='degrees C', aggregations=['min', 'max']),
             VariableSchemaAsciiDataFileReaderColumn(prod_flag_bit_index=6, name='boss_sliding', np_type=np.double,
-                                                    unit='degrees C', aggregations=['MIN', 'MAX'])
+                                                    unit='degrees C', aggregations=['min', 'max'])
             # prod_flag_bit_index = 7 is undefined
         ]
 
