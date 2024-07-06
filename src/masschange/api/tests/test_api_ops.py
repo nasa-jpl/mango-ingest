9b33a822ef8e9ede5a98008e4ff66a68c1b34b1d
ira <ira@jpl.nasa.gov>
2024-07-05 18:17:56 -0700

    190-create-reader-for-tnk1b

--- src/masschange/api/tests/test_api_ops.py
+++ src/masschange/api/tests/test_api_ops.py
@@ -32,6 +32,7 @@ def test_gracefo_data_select(ds: TimeSeriesDataset):
     # datasets containing multiple distinct time-series require additional parameters to identify a single time-series
     additional_parameters = {
         'TNK1A': '&filter=tank_id=1',
+        'TNK1B': '&filter=tank_id=1',
         'SCA1A': '&filter=sca_id=1',
         'SCA1B': '&filter=sca_id=1',
         'IMU1A': '&filter=gyro_id=1',
