039226f8a267b1780d987183b992f301f06473e1
ira <ira@jpl.nasa.gov>
2024-07-30 08:49:51 -0700

    202-create-llg1a-reader

---
+++ src/masschange/dataproducts/implementations/gracefo/primary/llg1a.py
@@ -0,0 +1,29 @@
+from datetime import timedelta
+
+from masschange.ingest.datafilereaders.base import DataFileReader
+from masschange.ingest.datafilereaders.gracefo.primary.llg1a import GraceFOLlg1ADataFileReader
+from masschange.missions import GraceFO
+from masschange.dataproducts.timeseriesdataproduct import TimeSeriesDataProduct
+
+
+class GraceFOLlg1ADataProduct(TimeSeriesDataProduct):
+    @classmethod
+    def get_reader(cls) -> DataFileReader:
+        return GraceFOLlg1ADataFileReader()
+
+    mission = GraceFO
+    id_suffix = 'LLG1A'
+    instrument_ids = {'C', 'D'}
+    time_series_interval = timedelta(seconds=1)
+    processing_level = '1A'
+
+    @classmethod
+    def get_sql_table_schema(cls) -> str:
+        return f"""
+            rcv_time bigint not null,
+            pkt_count int not null,
+            
+            GRACEFO_id CHAR not null,
+            logpacket VARCHAR(1000),
+            timestamp timestamptz not null
+        """
