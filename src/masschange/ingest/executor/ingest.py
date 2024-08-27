b8b4123a8e19da30b5873737441b15a39aaad40c
edunn <alexander.e.dunn@jpl.nasa.gov>
2024-08-27 13:41:06 -0700

    move masschange.ingest.executor.utils.enumeration under masschange.ingest.crawler

--- src/masschange/ingest/executor/ingest.py
+++ src/masschange/ingest/executor/ingest.py
@@ -19,7 +19,7 @@ from masschange.dataproducts.db.utils import get_db_connection
 from masschange.utils.misc import get_human_readable_elapsed_since
 from masschange.db.data.caggs import refresh_continuous_aggregates
 from masschange.db.ensure import ensure_table_exists, ensure_continuous_aggregates, ensure_database_exists, ensure_metadata_tables_exist
-from masschange.ingest.executor.utils.enumeration import enumerate_files_in_dir_tree, order_filepaths_by_filename
+from masschange.ingest.crawler.enumeration import enumerate_files_in_dir_tree, order_filepaths_by_filename
 from masschange.db.metadata.update import update_metadata
 from masschange.utils.logging import configure_root_logger
 from masschange.utils.timespan import TimeSpan
