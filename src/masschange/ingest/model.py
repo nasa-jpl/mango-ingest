b9e53ff70fd21e2770bc35291816a8b9cddf4ace
edunn <alexander.e.dunn@jpl.nasa.gov>
2024-08-28 16:37:40 -0700

    implement ingestmanagement model

---
+++ src/masschange/ingest/model.py
@@ -0,0 +1,18 @@
+"""
+Contains a crude set of ORM classes
+"""
+from dataclasses import dataclass, KW_ONLY
+from datetime import datetime
+from pathlib import Path
+from typing import Union
+
+
+@dataclass
+class JobManagerEntry:
+    _: KW_ONLY
+    id: int
+    src_filepath: Path
+    crawled_at: Union[datetime, None]
+    ingestion_started_at: Union[datetime, None]
+    ingestion_terminated_at: Union[datetime, None]
+    ingestion_error_msg: Union[str, None]
