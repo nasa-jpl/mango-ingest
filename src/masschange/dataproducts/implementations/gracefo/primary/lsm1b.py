cea9401d85f3947b7d3f354e6dd542dcae3ed1b1
ira <ira@jpl.nasa.gov>
2024-09-05 15:55:41 -0700

    261-create-reader-for-lsm1b

--- src/masschange/dataproducts/implementations/gracefo/primary/lsm1b.py
+++ src/masschange/dataproducts/implementations/gracefo/primary/lsm1b.py
@@ -1,33 +1,31 @@
 from datetime import timedelta
 
 from masschange.ingest.executor.datafilereaders.base import DataFileReader
-from masschange.ingest.executor.datafilereaders.gracefo.primary.lsm1a import GraceFOLsm1ADataFileReader
+from masschange.ingest.executor.datafilereaders.gracefo.primary.lsm1b import GraceFOLsm1BDataFileReader
 from masschange.missions import GraceFO
 from masschange.dataproducts.timeseriesdataproduct import TimeSeriesDataProduct
 
 
-class GraceFOLsm1ADataProduct(TimeSeriesDataProduct):
+class GraceFOLsm1BDataProduct(TimeSeriesDataProduct):
     @classmethod
     def get_reader(cls) -> DataFileReader:
-        return GraceFOLsm1ADataFileReader()
+        return GraceFOLsm1BDataFileReader()
 
     mission = GraceFO
-    id_suffix = 'LSM1A'
+    id_suffix = 'LSM1B'
     instrument_ids = {'C', 'D'}
     time_series_interval = timedelta(seconds=1/10)
-    processing_level = '1A'
+    processing_level = '1B'
 
     @classmethod
     def get_sql_table_schema(cls) -> str:
         return f"""
             time_intg bigint not null,
             time_frac int not null,
-            
             GRACEFO_id CHAR not null,
-            internalSensor0 int not null,
-            internalSensor1 int not null,
-            commanded0 int not null,
-            commanded1 int not null,
+            
+            lof_yaw double precision not null,
+            lof_pitch double precision not null,
             qualflg VARCHAR(8) not null,
             
             timestamp timestamptz not null
