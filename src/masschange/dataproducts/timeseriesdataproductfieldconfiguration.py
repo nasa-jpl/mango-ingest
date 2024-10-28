7d0c5a9272d3f9538374ad8b89608e3e1921edb1
edunn <alexander.e.dunn@jpl.nasa.gov>
2024-10-28 09:51:28 -0700

    add warning thresholds and effective time bounds to TimeSeriesDataProductFieldConfiguration

--- src/masschange/dataproducts/timeseriesdataproductfieldconfiguration.py
+++ src/masschange/dataproducts/timeseriesdataproductfieldconfiguration.py
@@ -8,15 +8,30 @@ class TimeSeriesDataProductFieldConfiguration:
     Contains configurable (i.e. stateful, stored in db) metadata relating to a TimeSeriesDataProductField.
     The only currently-known example of this is an optional min and max value for validity-checking
     """
+    effective_from: Union[datetime, None]
+    effective_to: Union[datetime, None]
     min_valid_value: Union[Any, None]
     max_valid_value: Union[Any, None]
+    lower_warn_threshold: Union[Any, None]
+    upper_warn_threshold: Union[Any, None]
 
-    def __init__(self, min_valid_value, max_valid_value):
+    def __init__(self,
+                min_valid_value: Union[Any, None] = None, lower_warn_threshold: Union[Any, None] = None,
+                upper_warn_threshold: Union[Any, None] = None, max_valid_value: Union[Any, None] = None,
+                effective_from: Union[datetime, None] = None, effective_to: Union[datetime, None] = None):
+        if effective_from is not None and effective_to is not None:
+            if not effective_to > effective_from:
+                raise ValueError(f'Cannot instantiate TimeSeriesDataProductFieldConfiguration where effective_to <= effective_from (got {effective_to}, {effective_from}, respectively)')
+
+        self.effective_from = effective_from
+        self.effective_to = effective_to
         self.min_valid_value = min_valid_value
         self.max_valid_value = max_valid_value
+        self.lower_warn_threshold = lower_warn_threshold
+        self.upper_warn_threshold = upper_warn_threshold
 
     @staticmethod
-    def construct(product, field) -> TimeSeriesDataProductFieldConfiguration:
+    def construct(product, field, effective_from: Union[datetime, None] = None, effective_to: Union[datetime, None] = None) -> TimeSeriesDataProductFieldConfiguration:
         """
         This is a stub to instantiate plausible min/max values for those types which are intended to be supported.
         TODO: In the future, this will fetch the values, if present, from the database.  It will be necessary to create
@@ -24,13 +39,13 @@ class TimeSeriesDataProductFieldConfiguration:
          database query.
         """
         if field.python_type is int:
-            return TimeSeriesDataProductFieldConfiguration(-1e10, 1e10)
+            return TimeSeriesDataProductFieldConfiguration(-1e10, -1e5, 1e5, 1e10, effective_from, effective_to)
         elif field.python_type is float:
-            return TimeSeriesDataProductFieldConfiguration(-1e10, float(1e10))
+            return TimeSeriesDataProductFieldConfiguration(float(-1e10), float(-1e5), float(1e5), float(1e10), effective_from, effective_to)
         elif field.python_type is datetime.datetime:
-            return TimeSeriesDataProductFieldConfiguration(datetime.datetime.min, datetime.datetime.max)
+            return TimeSeriesDataProductFieldConfiguration(datetime.datetime.min, datetime.datetime.max, effective_from=effective_from, effective_to=effective_to)
         elif field.python_type is datetime.date:
-            return TimeSeriesDataProductFieldConfiguration(datetime.date.min, datetime.date.max)
+            return TimeSeriesDataProductFieldConfiguration(datetime.date.min, datetime.date.max, effective_from=effective_from, effective_to=effective_to)
         else:
             raise ValueError(
                 f'python type "{field.python_type.__name__}" not in supported python types (int, float, datetime, date)')
