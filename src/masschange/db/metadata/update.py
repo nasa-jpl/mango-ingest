9f634d7a4b424de0f31b4f08c781db4ed99419f8
edunn <alexander.e.dunn@jpl.nasa.gov>
2024-11-21 09:36:37 -0800

    add comments

--- src/masschange/db/metadata/update.py
+++ src/masschange/db/metadata/update.py
@@ -7,12 +7,24 @@ from masschange.utils.timespan import TimeSpan
 
 
 def update_metadata(dataset: TimeSeriesDataset,
-                    data_span: Union[TimeSpan, None] = None, populate_versions = False):
+                    data_span: Union[TimeSpan, None] = None,
+                    populate_versions = False):
+    """
+
+    :param dataset: the dataset for which to update metadata
+    :param data_span: the data span, if any, to update the data with
+    :param populate_versions: not yet implemented - currently unclear what this was intended to accomplish
+           already present in the db.  If False, overwrite the span with the provided data_span.
+    :return:
+    """
     if populate_versions:
         raise NotImplementedError(f'update_metadata() does not yet support populate_versions - go ahead and implement population of queries from available table names')
 
-    with get_db_cursor() as cur:
+    # TODO: Consider cleaning up these calls - they're more readable, but the "retrieve" calls could be avoided by
+    #  putting the conditions into the dataset UPDATE query. Negligible performance impact, so tabled for now.
 
+    # Ensure product exists in db
+    with get_db_cursor() as cur:
         sql = """
             INSERT INTO _meta_dataproducts
             VALUES (DEFAULT, %(name)s, %(label)s)
@@ -20,6 +32,7 @@ def update_metadata(dataset: TimeSeriesDataset,
             """
         cur.execute(sql, {'name': dataset.product.get_full_id(), 'label': dataset.product.get_full_id()})
 
+    # Ensure instrument exists in db
     with get_db_cursor() as cur:
         sql = """
             INSERT INTO _meta_instruments
@@ -28,6 +41,7 @@ def update_metadata(dataset: TimeSeriesDataset,
             """
         cur.execute(sql, {'name': dataset.instrument_id, 'label': dataset.instrument_id})
 
+    # Retrieve product row id
     with get_db_cursor() as cur:
         sql = """
             SELECT id
@@ -37,6 +51,7 @@ def update_metadata(dataset: TimeSeriesDataset,
         cur.execute(sql, {'name': dataset.product.get_full_id()})
         data_product_db_id = cur.fetchone()[0]
 
+    # Retrieve intrument row id
     with get_db_cursor() as cur:
         sql = """
             SELECT id
@@ -46,6 +61,7 @@ def update_metadata(dataset: TimeSeriesDataset,
         cur.execute(sql, {'name': dataset.instrument_id})
         instrument_db_id = cur.fetchone()[0]
 
+    # Ensure versioned product exists in db
     with get_db_cursor() as cur:
         sql = """
             INSERT INTO _meta_dataproducts_versions
@@ -54,6 +70,7 @@ def update_metadata(dataset: TimeSeriesDataset,
             """
         cur.execute(sql, {'dataproduct': data_product_db_id, 'name': str(dataset.version), 'label': str(dataset.version)})
 
+    # Retrieve versioned product row id
     with get_db_cursor() as cur:
         sql = """
             SELECT id
@@ -63,6 +80,7 @@ def update_metadata(dataset: TimeSeriesDataset,
         cur.execute(sql, {'dataproduct': data_product_db_id, 'name': str(dataset.version)})
         dataproducts_versions_id = cur.fetchone()[0]
 
+    # Ensure dataset exists in db
     with get_db_cursor() as cur:
         sql = """
             INSERT INTO _meta_dataproducts_versions_instruments
@@ -73,6 +91,7 @@ def update_metadata(dataset: TimeSeriesDataset,
         cur.execute(sql,
                     {'dataproduct_version': dataproducts_versions_id, 'instrument': instrument_db_id})
 
+    # Set dataset metadata according to arguments
     if data_span is not None:
         with get_db_cursor() as cur:
             sql = """
