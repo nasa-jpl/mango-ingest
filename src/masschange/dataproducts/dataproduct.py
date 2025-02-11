816a17a12ecd8a80da5dcac9bb1efd9a04b920da
edunn <alexander.e.dunn@jpl.nasa.gov>
2025-02-11 11:48:13 -0800

    bump DataProduct max query span 31 days -> 10 years this is to assist in development and can be made more strict if required

--- src/masschange/dataproducts/dataproduct.py
+++ src/masschange/dataproducts/dataproduct.py
@@ -234,7 +234,7 @@ class DataProduct(ABC):
 
     @staticmethod
     def get_max_query_temporal_span(downsampling_factor: int) -> timedelta:
-        return timedelta(days=31)  # TODO: set to 31 days for now
+        return timedelta(days=365 * 10)  # TODO: set to 10 years for now for development - ***REMOVED***
 
     @classmethod
     def get_available_aggregation_levels(cls) -> Sequence[int]:
