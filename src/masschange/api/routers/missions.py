4f9ac692cbf948738a130c1883e8a3f55950e1bf
edunn <alexander.e.dunn@jpl.nasa.gov>
2025-03-20 15:08:26 -0700

    add channel-id enums to monolithic metadata route response content

--- src/masschange/api/routers/missions.py
+++ src/masschange/api/routers/missions.py
@@ -1,8 +1,9 @@
+import logging
 from typing import Iterable, Type
 
 from fastapi import APIRouter, HTTPException
 
-from masschange.api.utils.db.queries import fetch_bulk_metadata
+from masschange.api.utils.db.queries import fetch_bulk_metadata, fetch_bulk_channel_id_enums
 from masschange.dataproducts.utils import get_dataproduct_classes, get_dataproducts
 from masschange.missions import Mission
 
@@ -22,7 +23,15 @@ def get_available_data_products_for_mission(mission_id: str):
         raise HTTPException(status_code=400,
                             detail=f'No mission found with id {mission_id} in extant missions ({sorted(mission.id for mission in available_missions)})')
     # use metadata cache to enable population of datasets with full metadata
-    metadata_cache = list(fetch_bulk_metadata())
+    # TODO: Clean up the keying system here to use TimeSeriesDataSets rather than id-keyed or keyless list elements - ***REMOVED*** 20250320
+    metadata_cache = fetch_bulk_metadata()
+    for dataset_id, channel_id_enums in fetch_bulk_channel_id_enums().items():
+        try:
+            metadata_cache[dataset_id]['channel_id_enums'] = channel_id_enums
+        except KeyError as err:
+            logging.warning(f'Failed to assign channel-id metadata for dataset with id "{dataset_id}": {err}')
+    metadata_cache = list(metadata_cache.values())
+
     mission_data_products = [p for p in get_dataproducts() if p.mission.id == mission_id]
     return {'data': [product.describe(metadata_cache=metadata_cache) for product in
                      sorted(mission_data_products, key=lambda product: product.id_suffix)]}
--- src/masschange/api/utils/db/queries.py
+++ src/masschange/api/utils/db/queries.py
@@ -1,4 +1,6 @@
 import logging
+from collections.abc import Collection
+from typing import Dict, Set
 
 import psycopg2
 
@@ -29,4 +31,55 @@ JOIN _meta_instruments mi on mi.id = mdpvi._meta_instruments_id
         except Exception as err:
             logging.warning(f'query failed with {err}: {sql}')
             return None
+    return {f"{row['product']}_{row['version']}_{row['instrument']}": row for row in results}
+
+
+def fetch_bulk_channel_id_enums() -> Dict[str, Dict[str, Collection[str]]]:
+    """
+    Normally, fetching channel-id enum values requires one query per dataset, but that is unnecessarily slow if many
+    datasets are desired.
+
+    This optimized query returns channel-id enum value dicts for all datasets, keyed by dataset full-id, then field name
+
+    Strictly-speaking, this is partially-redundant with fetch_bulk_metadata() and could be merged, but this is only worthwhile if
+    there is a tangible performance impact observed.
+    """
+
+    with get_db_cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+        try:
+            sql = f"""
+    SELECT mdp.name as product_id, mdv.name as version_id, mi.name as instrument_id, field_name, value
+    FROM _meta_datasets_channelidvalues as civ
+        JOIN _meta_dataproducts_versions_instruments datasets on datasets.id = civ.dataset_id
+        JOIN _meta_dataproducts_versions mdv on mdv.id = datasets._meta_dataproducts_versions_id
+        JOIN _meta_dataproducts mdp on mdp.id = mdv._meta_dataproducts_id
+        JOIN _meta_instruments mi on datasets._meta_instruments_id = mi.id;
+                            """
+            cur.execute(sql)
+            result_rows = cur.fetchall()
+        except Exception as err:
+            logging.warning(f'query failed with {err}: {sql}')
+            return None
+
+    results = {}
+    for row in result_rows:
+        dataset_id = f'{row["product_id"]}_{row["version_id"]}_{row["instrument_id"]}'
+        field_name = row['field_name']
+        value = row['value']
+
+        if dataset_id not in results:
+            results[dataset_id] = {}
+
+        channel_id_enums: Dict[str, Set[str]] = results[dataset_id]
+        if field_name not in channel_id_enums:
+            channel_id_enums[field_name] = set()
+
+        field_enum_values: Set[str] = channel_id_enums[field_name]
+        field_enum_values.add(value)
+
+    # Sort the enums for clean presentation in API response
+
+    for dataset_id in results.keys():
+        for field_name in results[dataset_id].keys():
+            results[dataset_id][field_name] = sorted(results[dataset_id][field_name])
     return results
