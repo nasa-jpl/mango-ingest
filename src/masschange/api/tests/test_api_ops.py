556ff0a1052ce5342dfdb260c5fc26bed36dc439
edunn <alexander.e.dunn@jpl.nasa.gov>
2025-03-26 17:24:40 -0700

    Revert "add 'qc_thresholds' to dataset metadata expected"
    This reverts commit 2dd3330e18093b238a10c6617958855ae4bc9fc9 and is necessary as presence of qc_thresholds is not guaranteed (i.e. if no thresholds exist)

--- src/masschange/api/tests/test_api_ops.py
+++ src/masschange/api/tests/test_api_ops.py
@@ -241,12 +241,10 @@ def test_dataset_metadata(ds: TimeSeriesDataset):
         assert k in content
 
     expected_field_attributes = ['name', 'type', 'description', 'unit', 'supported_aggregations',
-                                 'is_channel_id', 'qc_thresholds']
+                                 'is_channel_id']
     for field in content['available_fields']:
         for k in expected_field_attributes:
             assert k in field
-
-        # TODO: enum_values will need to be removed or the reference updated when that is refactored to be a product property rather than a dataset property
         if field['is_channel_id'] is True:
             if 'enum_values' not in field:
                 logging.error(f'"enum_values" not in field {json.dumps(field)} from path {path}')
