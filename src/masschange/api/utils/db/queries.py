1c42711fe1e344878a811449547ec45d72e71f67
edunn <alexander.e.dunn@jpl.nasa.gov>
2025-04-16 16:56:46 -0700

    overhaul bulk metadata queries (typing/keying, and updating channel-id enum values bulk fetch for product-level values)

--- src/masschange/api/utils/db/queries.py
+++ src/masschange/api/utils/db/queries.py
@@ -4,11 +4,13 @@ from typing import Dict, Set
 
 import psycopg2
 
-from masschange.api.tests.utils import permute_all_datasets
+from masschange.dataproducts.dataproduct import DataProduct
+from masschange.dataproducts.utils import get_dataproducts
 from masschange.db.conn import get_db_cursor
 
 
-def fetch_bulk_metadata():
+def fetch_dataset_bulk_metadata() -> Dict[str, Dict]:
+    # TODO: incorporate CachedDatasetMetadata objects in output rather than using dicts
     """
     Normally, fetching dataset metadata requires one query per dataset, but that is unnecessarily slow if many
     datasets are desired.
@@ -31,19 +33,18 @@ JOIN _meta_instruments mi on mi.id = mdpvi._meta_instruments_id
             results = cur.fetchall()
         except Exception as err:
             logging.warning(f'query failed with {err}: {sql}')
-            return None
+            return {}
     return {f"{row['product']}_{row['version']}_{row['instrument']}": row for row in results}
 
 
-def fetch_bulk_channel_id_enums() -> Dict[str, Dict[str, Collection[str]]]:
+def fetch_bulk_channel_id_enums() -> Dict[DataProduct, Dict[str, Collection[str]]]:
+    # TODO: incorporate CachedDatasetMetadata objects in output rather than using dicts, and rename method to represent all product-level metadata
+
     """
-    Normally, fetching channel-id enum values requires one query per dataset, but that is unnecessarily slow if many
-    datasets are desired.
+    Normally, fetching channel-id enum values requires one query per product, but that is unnecessarily slow if many
+    products are desired.
 
-    This optimized query returns channel-id enum value dicts for all datasets, keyed by dataset full-id, then field name
-
-    Strictly-speaking, this is partially-redundant with fetch_bulk_metadata() and could be merged, but this is only worthwhile if
-    there is a tangible performance impact observed.
+    This optimized query returns channel-id enum value dicts for all products, keyed by product full-id, then field name
     """
 
     with get_db_cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
@@ -51,7 +52,7 @@ def fetch_bulk_channel_id_enums() -> Dict[str, Dict[str, Collection[str]]]:
             sql = f"""
     SELECT mdp.name as product_id, field_name, value
     FROM _meta_dataproducts_channelidvalues as civ
-        JOIN _meta_dataproducts mdp on mdp.id = civ.dataproducts_id
+        JOIN _meta_dataproducts mdp on mdp.id = civ.dataproduct_id
                             """
             cur.execute(sql)
             result_rows = cur.fetchall()
@@ -60,27 +61,27 @@ def fetch_bulk_channel_id_enums() -> Dict[str, Dict[str, Collection[str]]]:
             return None
 
     results = {}
-    # Initialise the results structure - this is necessary to ensure null-sets are created for datasets with no ingested
+    # Initialise the results structure - this is necessary to ensure null-sets are created for products with no ingested
     # data (and thus, no enum value rows in the metadata table)
-    for dataset in permute_all_datasets():
-        dataset_id = f'{dataset.product.get_full_id()}_{dataset.version}_{dataset.instrument_id}'
-        channel_id_fields = [f for f in dataset.product.get_available_fields() if f.is_channel_id_column]
-        results[dataset_id] = {}
+    for product in get_dataproducts():
+        channel_id_fields = [f for f in product.get_available_fields() if f.is_channel_id_column]
+        results[product] = {}
         for field in channel_id_fields:
-            results[dataset_id][field.name] = set()
+            results[product][field.name] = set()
 
     for row in result_rows:
-        dataset_id = f'{row["product_id"]}_{row["version_id"]}_{row["instrument_id"]}'
+        product_id = row['product_id']
+        product = next(product for product in get_dataproducts() if product.get_full_id() == product_id)
         field_name = row['field_name']
         value = row['value']
 
-        channel_id_enums: Dict[str, Set[str]] = results[dataset_id]
+        channel_id_enums: Dict[str, Set[str]] = results[product]
         field_enum_values: Set[str] = channel_id_enums[field_name]
         field_enum_values.add(value)
 
     # Sort the enums for clean presentation in API response
 
-    for dataset_id in results.keys():
-        for field_name in results[dataset_id].keys():
-            results[dataset_id][field_name] = sorted(results[dataset_id][field_name])
+    for product in results.keys():
+        for field_name in results[product].keys():
+            results[product][field_name] = sorted(results[product][field_name])
     return results
