50c2cbbc75f1d0810a632ef6408747428eea24e2
edunn <alexander.e.dunn@jpl.nasa.gov>
2025-05-08 16:03:53 -0700

    implement ingest.manager.filestatus

---
+++ src/masschange/ingest/manager/filestatus.py
@@ -0,0 +1,15 @@
+from enum import auto
+from strenum import StrEnum  # only supported in stdlib from Python 3.11 onward
+
+
+class FileStatus(StrEnum):
+    CRAWLED = auto()
+    STAGED = auto()
+    INGEST_STARTED = auto()
+    INGEST_SUCCESS = auto()
+    INGEST_TERMINATED = auto()
+
+    @property
+    def db_column_name(self) -> str:
+        """Return the name of the database column used to track the timestamp of each status change."""
+        return f'{self.lower()}_at'
