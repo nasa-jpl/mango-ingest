a06add99ecbe083a568241c13c891cd85e174300
ira <ira@jpl.nasa.gov>
2025-08-27 12:51:33 -0700

    Implemented MultipartFileDataProduct per PR change request

--- src/masschange/dataproducts/implementations/gracefo/offred/offred.py
+++ src/masschange/dataproducts/implementations/gracefo/offred/offred.py
@@ -1,14 +1,13 @@
 from datetime import timedelta
 
+from masschange.dataproducts.multipartfiledataproduct import TimeSeriesMultipartFileDataProduct
 from masschange.ingest.executor.datafilereaders.base import DataFileReader
 from masschange.ingest.executor.datafilereaders.gracefo.offred.offred import GraceFOOffredDataFileReader
 from masschange.missions import GraceFO
-from masschange.dataproducts.timeseriesdataproduct import TimeSeriesDataProduct
 
 
-class GraceFOOffredDataProduct(TimeSeriesDataProduct):
-    DELETE_OVERLAP_ON_INGEST_BASED_ON_SOURCE_FILE_NAME = True   # if True, delete duplicates based on source file name,
-                                                                # rather than time range.
+class GraceFOOffredDataProduct(TimeSeriesMultipartFileDataProduct):
+
     @classmethod
     def get_reader(cls) -> DataFileReader:
         return GraceFOOffredDataFileReader()
@@ -18,12 +17,10 @@ class GraceFOOffredDataProduct(TimeSeriesDataProduct):
     instrument_ids = {'GF1', 'GF2'}
     # TODO: frequency is different per type of data. Need to figure out optimal common frequency
     time_series_interval = timedelta(seconds=30)
-    processing_level = '1A' # TODO: confirm it. May be it is 'None'
+    processing_level = '0' # TODO: confirm it. May be it is 'None'
 
     @classmethod
     def get_sql_table_schema(cls) -> str:
-        # NOTE: qualflag bit 7 = No ICU block number available for GRACE-FO,
-        # so assume that icu_blk_nr could be NULL
         return f"""
             utc VARCHAR(21) not null,  
             obt_integer bigint not null, 
