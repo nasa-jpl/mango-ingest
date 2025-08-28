1e7d4f9c64ec454757e1d8976ca9c79b9a2cb6f0
edunn <alexander.e.dunn@jpl.nasa.gov>
2025-08-28 10:26:10 -0700

    implement Mission.label

--- src/masschange/missions/__init__.py
+++ src/masschange/missions/__init__.py
@@ -1,6 +1,8 @@
 class Mission:
     """Contains basic mission-wide configuration used by member datasets"""
     id: str
+    label: str
 
 class GraceFO(Mission):
     id = 'GRACEFO'
+    label = 'GRACE-FO'
