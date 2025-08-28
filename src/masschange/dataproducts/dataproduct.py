265302818fa4b0b398597642f1c6d37296cf119d
IrinaTkatcheva <irina.n.tkatcheva@jpl.nasa.gov>
2025-08-28 12:20:20 -0700

    Update dataproduct.py

--- src/masschange/dataproducts/dataproduct.py
+++ src/masschange/dataproducts/dataproduct.py
@@ -322,4 +322,3 @@ class DataProduct(ABC):
                 ON CONFLICT DO NOTHING;
                 """
             cur.execute(sql, {'name': cls.get_full_id(), 'label': cls.get_full_id()})
-            
