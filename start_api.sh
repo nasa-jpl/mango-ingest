4b05a307a8622ed70a3f265fda7a598ee5a47860
edunn <alexander.e.dunn@jpl.nasa.gov>
2025-10-02 13:52:11 -0700

    ensure logs reach docker logs capture

--- start_api.sh
+++ start_api.sh
@@ -2,14 +2,14 @@ mkdir -p /logs/api
 
 #Start API server
 if [[ "${API_ENABLE_SSL:-0}" != "0" ]]; then
-  uvicorn masschange.api.app:app \
+  python -u -m uvicorn masschange.api.app:app \
     --root-path "$API_ROOT_PATH" \
     --host 0.0.0.0 \
     --ssl-keyfile=/certs/ssl.key \
     --ssl-certfile=/certs/ssl.crt \
     --log-config "$MASSCHANGE_REPO_ROOT"/src/masschange/api/log.ini
 else
-  uvicorn masschange.api.app:app \
+  python -u -m uvicorn masschange.api.app:app \
     --root-path "$API_ROOT_PATH" \
     --host 0.0.0.0 \
     --log-config "$MASSCHANGE_REPO_ROOT"/src/masschange/api/log.ini
