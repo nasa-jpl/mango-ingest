65a7eae4579af7f882cf1022556479af435b957d
ira <ira@jpl.nasa.gov>
2025-10-22 16:52:23 -0700

    Updated to have a single version '00'; added 'subset_version' column to keep the subset version from the file name

--- tests/ingest/datasets/gracefo/primary/test_icsnr.py
+++ tests/ingest/datasets/gracefo/primary/test_icsnr.py
@@ -9,13 +9,13 @@ class GraceFOIcsnrDatasetReaderTestCase(DatasetReaderTestCaseBase):
     data_is_zipped = False
     dataset_cls = GraceFOIcsnrDataProduct
     expected_table_names = ['gracefo_icsnr_00_c', 'gracefo_icsnr_00_d']
-    expected_field_types = [float, float, float, float, datetime]
-    expected_table_row_counts = [101,101]
+    expected_field_types = [float, float, float, float, str, datetime]
+    expected_table_row_counts = [200,200]
     expected_table_first_rows = [
-        (738849609.0, 0.05265774577856064, 784.0, 774.0,
-         datetime(2023, 6, 1, 0, 0, 9, tzinfo=timezone.utc)),
-        (738849609.0, 0.05418538302183151, 780.0, 767.0,
-         datetime(2023, 6, 1, 0, 0, 9, tzinfo=timezone.utc))
+        (739022409.0, -0.025890573859214783, 781.0, 767.0, '00',
+         datetime(2023, 6, 3, 0, 0, 9, tzinfo=timezone.utc)),
+        (739022409.0, -0.025527916848659515, 776.0, 760.0, '00',
+         datetime(2023, 6, 3, 0, 0, 9, tzinfo=timezone.utc))
     ]
 if __name__ == '__main__':
     unittest.main()
