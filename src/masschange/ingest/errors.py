c3ae8f5c3adc7abb3bcbb47370abcebb49eb154e
edunn <alexander.e.dunn@jpl.nasa.gov>
2026-03-11 16:17:44 -0700

    squash! implement ZeroByteFileError revert

--- src/masschange/ingest/errors.py
+++
@@ -1,5 +0,0 @@
-class ZeroByteFileError(ValueError):
-    """
-    Raised when a zero-byte file is encountered.  This is distinct from the valid case where a primary product file with
-    zero data rows (but with the correct header) is encountered.
-    """
