18d020a85d2ed503f1da3211e3d9d5e83af58c12
edunn <alexander.e.dunn@jpl.nasa.gov>
2026-03-25 14:33:56 -0700

    add todo: remove JM table name hardcoding

--- src/masschange/db/constants/tablenames.py
+++ src/masschange/db/constants/tablenames.py
@@ -1,5 +1,5 @@
 """
 Defines static database table names for use throughout project
 """
-
+# TODO: remove remaining hardcoded references to this table name throughout codebase
 INGEST_MANAGER_TABLE_NAME = '_ingestmgr_crawled_files'
