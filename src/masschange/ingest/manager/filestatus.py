84d2e02650ab6111b5eaacbb43d8dea77ff4e954
edunn <alexander.e.dunn@jpl.nasa.gov>
2026-03-26 12:01:34 -0700

    bandaid + todo

--- src/masschange/ingest/manager/filestatus.py
+++ src/masschange/ingest/manager/filestatus.py
@@ -14,4 +14,12 @@ class FileStatus(StrEnum):
     def db_column_name(self) -> str:
         """Return the name of the database column used to track the timestamp of each status change."""
         # TODO: EXCISE THIS - PRONE TO ERROR
+        # TODO 2: iron out the distinction between success, termination, etc
+        #  currently, the fact that success is tracked with INGEST_TERMINATED is causing brittleness
+        #  suggest defining "terminated" as "ended, unsuccessfully" and tracking ingest_success_at separately
+
+        #### Begin temporary bandaid to address TODO 2 issues
+        if self == FileStatus.INGEST_SUCCESS:
+            return FileStatus.INGEST_TERMINATED.db_column_name
+
         return f'{self.lower()}_at'
