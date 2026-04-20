8e608521eca3ab1752627e038c3dc6bfdeab51b7
edunn <alexander.e.dunn@jpl.nasa.gov>
2026-04-20 10:32:46 -0700

    first-pass implementation of SSL context wrapper this is to comply with security directives for high-strength ciphers only

--- start_api.sh
+++ start_api.sh
@@ -1,12 +1,22 @@
 mkdir -p /logs/api
 
-#Start API server
+CIPHER_SUITE="ECDHE-ECDSA-AES256-GCM-SHA384"
+CIPHER_SUITE+=":ECDHE-ECDSA-AES128-GCM-SHA256"
+CIPHER_SUITE+=":ECDHE-ECDSA-AES256-SHA384"
+CIPHER_SUITE+=":ECDHE-ECDSA-AES128-SHA256"
+CIPHER_SUITE+=":ECDHE-RSA-AES256-GCM-SHA384"
+CIPHER_SUITE+=":ECDHE-RSA-AES128-GCM-SHA256"
+CIPHER_SUITE+=":ECDHE-RSA-AES256-SHA384"
+CIPHER_SUITE+=":ECDHE-RSA-AES128-SHA256"
+CIPHER_SUITE+=":!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK"
+
 if [[ "${API_ENABLE_SSL:-0}" != "0" ]]; then
   python -u -m uvicorn masschange.api.app:app \
     --root-path "$API_ROOT_PATH" \
     --host 0.0.0.0 \
     --ssl-keyfile=/certs/ssl.key \
     --ssl-certfile=/certs/ssl.crt \
+    --ssl-ciphers="$CIPHER_SUITE" \
     --log-config "$MASSCHANGE_REPO_ROOT"/src/masschange/api/log.ini
 else
   python -u -m uvicorn masschange.api.app:app \
