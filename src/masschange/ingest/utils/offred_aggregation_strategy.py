09e0c46f7e38cd55755af5dabdd323886f895924
ira <ira@jpl.nasa.gov>
2026-06-30 11:26:49 -0700

    Ingest_unzipped_OFFRED_files

--- src/masschange/ingest/utils/offred_aggregation_strategy.py
+++ src/masschange/ingest/utils/offred_aggregation_strategy.py
@@ -2,5 +2,4 @@
 from enum import Enum
 class OffredAggregationStrategy(Enum):
     NONE = "NONE"             # 1. Do not aggregate
-    AFTER_ALL = "AFTER_ALL"   # 2. Aggregate after all files in zip file are ingested
-    AFTER_EACH = "AFTER_EACH" # 3. Aggregate after each individual un-zipped file
+    AFTER_EACH = "AFTER_EACH" # 2. Aggregate after each individual un-zipped file
