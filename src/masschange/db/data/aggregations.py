4ab56cbe150bdd97d911ddad01173c91e97f2d39
edunn <alexander.e.dunn@jpl.nasa.gov>
2026-09-01 13:38:17 -0700

    [bugfix] only apply null-filter optimisation to TrivialAggregation

--- src/masschange/db/data/aggregations.py
+++ src/masschange/db/data/aggregations.py
@@ -1,23 +1,36 @@
 from abc import ABC
-from typing import Sequence, Callable, Dict
+from typing import Sequence, Callable, Dict, Optional
 
 
 class Aggregation(ABC):
     """Defines an aggregation on a single column and provides utilities for interfacing with SQL"""
 
-    def __init__(self, name: str, sql_expr_f: Callable[[str], str], output_name_f: Callable[[str], str]):
+    def __init__(self, name: str, output_name_f: Callable[[str], str]):
         self._name = name
-        self._sql_expr_f = sql_expr_f
         self._output_name_f = output_name_f
 
     @property
     def name(self) -> str:
+        """
+        The label/name for the aggregation - used to identify aggregations in the API and to specify a friendly label
+        for NestedAggregations
+        """
         return self._name
 
     def get_sql_expression(self, operand_column_name: str) -> str:
-        return f'{self._sql_expr_f(operand_column_name)} FILTER (WHERE {operand_column_name} IS NOT NULL)'
+        """
+        Get the SQL expression for the aggregation applied to a given column.
+        :param operand_column_name:
+        :return:
+        """
+        raise NotImplementedError
 
     def get_aggregated_name(self, operand_column_name: str) -> str:
+        """
+        Get the resultant column label when the aggregation is applied to a given column.
+        :param operand_column_name:
+        :return:
+        """
         return self._output_name_f(operand_column_name)
 
     def describe(self, operand_column_name: str) -> Dict:
@@ -34,9 +47,15 @@ class TrivialAggregation(Aggregation):
     and names the output by appending the aggregation function name to the column.
     """
 
-    def __init__(self, func_name: str):
-        super().__init__(func_name, lambda column_name: f'{func_name}({column_name})',
-                         lambda column_name: f'{column_name}_{func_name.lower()}')
+    def __init__(self, func_name: str, filter_nulls: bool = True):
+        self._func_name = func_name
+        self.filter_nulls = filter_nulls
+
+        super().__init__(func_name, lambda column_name: f'{column_name}_{func_name.lower()}')
+
+    def get_sql_expression(self, operand_column_name: str) -> str:
+        filter_expr_if_exists = f'FILTER (WHERE {operand_column_name} IS NOT NULL)' if self.filter_nulls else ''
+        return f'{self._func_name}({operand_column_name}) {filter_expr_if_exists}'
 
 
 class NestedAggregation(Aggregation):
@@ -48,17 +67,16 @@ class NestedAggregation(Aggregation):
     correct construction would be NestedAggregation(["F2", "F1"])
     """
 
-    def __init__(self, name, func_names: Sequence[str], output_name_f_override: Callable[[str], str] = None):
+    def __init__(self, name, func_names: Sequence[str], output_name_f_override: Optional[Callable[[str], str]] = None):
         self._func_names = func_names
 
         super().__init__(
             name,
-            self._compose_sql_expr,
             output_name_f_override if output_name_f_override is not None else lambda column_name: column_name
         )
 
-    def _compose_sql_expr(self, column_name: str) -> str:
-        expr = column_name
+    def get_sql_expression(self, operand_column_name: str) -> str:
+        expr = operand_column_name
         for func_name in self._func_names:
             expr = f'{func_name}({expr})'
         return expr
